222a6d2b89b36d8a6f5c407b54ed8b3e4e508cc1
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-02 16:04:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-// Get references to HTML elements
-const textInput = document.getElementById('text');
-const delayInput = document.getElementById('delay');
-const btn = document.getElementById('btn');
-const outputDiv = document.getElementById('output');
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
 
-// Define the async function
-async function displayMessage() {
-  // Get the text and delay values from the inputs
-  const text = textInput.value;
-  const delay = parseInt(delayInput.value);
-
-  // Wait for the specified delay
-  await new Promise(resolve => setTimeout(resolve, delay));
-
-  // Display the message on the webpage
-  outputDiv.textContent = text;
+//your code here
+async function showMessage() {
+  const message = text.value;
+  const delayVal = delay.value;
+  await new Promise((resolve) => setTimeout(resolve, delayVal));
+  output.innerText = message;
 }
 
-// Add event listener to the button
-btn.addEventListener('click', displayMessage);
+btn.addEventListener("click", showMessage);
